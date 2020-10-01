f7b6e678e2171b774f74e14053f60758ba5a9a35
Markay12 <ashinhust.brass@gmail.com>
2020-09-30 21:14:13 -0700

    updated logic, random num between 1 and 3 for questions

--- main.js
+++ main.js
@@ -174,12 +174,24 @@ function Farad(arguments, receivedMessage){
 
     if(arguments.length > 1){
 
-        var randomNum = Math.floor((Math.random() * 10) + 1)
+        var randomNum = Math.floor((Math.random() * 3) + 1)
 
-        if (randomNum > 0)
+        if (randomNum == 0)
         {
 
             receivedMessage.channel.send("How would I know you lousy son of a bitch")
+
+        }
+        if (randomNum == 1)
+        {
+
+            receivedMessage.channel.send("Wouldn't you like to know weather boy")
+
+        }
+        if (randomNum == 2)
+        {
+
+            receivedMessage.channel.send("You're an idiot")   
 
         }
 
