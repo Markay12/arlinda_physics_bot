5008080d1a598a57b1f5ee0b301202b0e944d012
Markay12 <ashinhust.brass@gmail.com>
2020-10-01 17:28:09 -0700

    new function for github repo

--- main.js
+++ main.js
@@ -134,6 +134,8 @@ function processCommand(receivedMessage) {
         divideCommand(arguments, receivedMessage)
     } else if (primaryCommand == "Farad" || primaryCommand == "farad"){
         Farad(arguments, receivedMessage)
+    } else if (primaryCommand == "sourceCode" || "source"){
+        git(arguments, receivedMessage)
     } else {
         receivedMessage.channel.send("I don't understand the command. Try `$help` or `$multiply`")
     }
@@ -193,6 +195,13 @@ function Farad(arguments, receivedMessage){
         receivedMessage.reply("Ask me a question... ")
 
     }
+
+}
+
+function git(arguments, receivedMessage)
+{
+
+    receivedMessage.author.send("Here is the link to the github repo --> https://github.com/Markay12/arlinda_physics_bot\n\n Let me know if you want any extra functionality\nYou shouldn't be able to change anything, but you can fork it and work on something if you wish\n\nWritten in Node JS and works with a large amount of functions\n\nContact Markay#5500 for assistance :smile:")
 
 }
 
